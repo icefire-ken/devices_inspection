a2c340001e33090fc47ebd08ef665e2893698874
icefire-ken <IceFire_Ken@163.com>
2021-11-11 13:03:49 +0800

    2021.11.11

--- devices_inspection.py
+++ devices_inspection.py
@@ -13,7 +13,7 @@ def get_devices_info(info_file):  # 获取信息文件中的设备登录信息
     try:
         devices_dataframe = pandas.read_excel(info_file, sheet_name=0)  # 读取excel文件第一张表格的数据生成DataFrame
         devices_dict = devices_dataframe.to_dict('records')  # 将DataFrame转换成字典
-        # 'records'参数规定外层为列表，内层以列标题为key，一行内容为value的字典
+        # 'records'参数规定外层为列表，内层以列标题为key，以此列的行内容为value的字典
         # 若有多列，代表字典内有多个key:value对；若有多行，每行为一个字典
         return devices_dict
     except (FileNotFoundError):  # 如果没有配置信息文件或信息文件名称错误
@@ -41,22 +41,22 @@ def inspection(device_info, cmds_dict):
         ssh.enable()  # 进入设备Enable模式
     except (AttributeError):  # 登录信息中缺失IP地址
         print(f'设备 {device_info["host"]} 登录信息错误！')  # 打印提示该设备登录信息错误
-        log = open(os.getcwd() + '\\' + localtime + '\\' + 'log.log', 'a')  # 创建log文件保存巡检报错的设备故障信息
+        log = open(os.getcwd() + '\\' + localtime + '\\' + '01log.log', 'a')  # 创建log文件保存巡检报错的设备故障信息
         log.write('设备 ' + device_info['host'] + ' 登录信息错误！\n')
         log.close()
     except (NetmikoTimeoutException):  # 登录信息中IP地址不可达
-        print(f'设备 {device_info["host"]} 管理地址不可达！')
-        log = open(os.getcwd() + '\\' + localtime + '\\' + 'log.log', 'a')
-        log.write('设备 ' + device_info['host'] + ' 管理地址不可达！\n')
+        print(f'设备 {device_info["host"]} 管理地址或端口不可达！')
+        log = open(os.getcwd() + '\\' + localtime + '\\' + '01log.log', 'a')
+        log.write('设备 ' + device_info['host'] + ' 管理地址或端口不可达！\n')
         log.close()
     except (AuthenticationException):  # 登录信息中用户名或密码错误
         print(f'设备 {device_info["host"]} 登录认证失败！')
-        log = open(os.getcwd() + '\\' + localtime + '\\' + 'log.log', 'a')
+        log = open(os.getcwd() + '\\' + localtime + '\\' + '01log.log', 'a')
         log.write('设备 ' + device_info['host'] + ' 登录认证失败！\n')
         log.close()
     except (ValueError):  # 登录信息中的Enable密码错误
         print(f'设备 {device_info["host"]} Enable密码错误！')
-        log = open(os.getcwd() + '\\' + localtime + '\\' + 'log.log', 'a')
+        log = open(os.getcwd() + '\\' + localtime + '\\' + '01log.log', 'a')
         log.write('设备 ' + device_info['host'] + ' Enable密码错误！\n')
         log.close()
     else:  # 如果登录正常，开始巡检
@@ -80,13 +80,13 @@ if __name__ == '__main__':
     thread_list = []  # 创建一个列表，准备存放所有线程
     devices_info = get_devices_info(info_path)  # 读取所有设备信息
     cmds_info = get_cmds_info(info_path)  # 读取所有设备类型的巡检命令
-    pool = threading.BoundedSemaphore(100)  # 最大巡检线程控制
+    pool = threading.BoundedSemaphore(300)  # 最大巡检线程控制
     print('巡检开始...')  # 提示巡检开始
     if not os.path.exists(localtime):  # 检查是否有同日期命名的相同文件夹
         os.makedirs(localtime)  # 如果没有，创建日期文件夹
     else:  # 如果有
         try:  # 尝试删除巡检故障设备报错的log文件
-            os.remove(os.getcwd() + '\\' + localtime + '\\' + 'log.log')  # 删除log文件
+            os.remove(os.getcwd() + '\\' + localtime + '\\' + '01log.log')  # 删除log文件
         except (FileNotFoundError):  # 如果没有log文件
             pass  # 跳过，不做处理
     for device_info in devices_info:  # 遍历所有设备登录信息
@@ -97,5 +97,12 @@ if __name__ == '__main__':
         pre_device.start()  # 开启这个线程
     for i in thread_list:  # 遍历所有创建的线程
         i.join()  # 等待所有线程的结束
+    try:  # 尝试打开巡检log文件
+        logfile = open(os.getcwd() + '\\' + localtime + '\\' + '01log.log')  # 打开巡检log文件
+    except (FileNotFoundError):  # 如果找不到巡检log文件
+        logfilelines = 0  # 证明没有出现巡检登录异常情况
+    else:  # 如果正常打开了巡检log文件
+        logfilelines = len(logfile.readlines())  # 读取巡检log文件共有多少行，有多少行，代表出现了多少个设备登录异常
     t2 = time.time()  # 程序执行计时结束点
-    print('巡检结束，共用时', round(t2 - t1, 1), '秒。')  # 打印程序执行共用时长
+    print('\n' + '=' * 30 + '\n')  # 打印一行“=”，隔开巡检报告信息
+    print(f'巡检结束，共巡检 {len(thread_list)} 台设备， {logfilelines} 台异常，共用时 {round(t2 - t1, 1)} 秒。')  # 打印巡检报告
