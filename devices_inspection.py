a5f7686cde5ceb2fbf26cf3d3a192534bf95204b
Ken.Tang <IceFire_Ken@163.com>
2024-01-19 17:10:34 +0800

    2024.01.19

--- devices_inspection.py
+++ devices_inspection.py
@@ -133,4 +133,3 @@ if __name__ == '__main__':
     print(f'\n' + '<' * 40 + '\n')  # 打印一行“<”，隔开巡检报告信息
     print(f'巡检结束，共巡检 {len(threading_list)} 台设备， {file_lines} 台异常，共用时 {round(t2 - t1, 1)} 秒。')  # 打印巡检报告
     time.sleep(3)  # 等待3秒退出程序，方便使用者查看CMD中的输出信息
-
